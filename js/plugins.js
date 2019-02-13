41844ac32ecc1b0eaa0a3d12f4eb1480f3d39609
Wok Seng <wok@ualberta.ca>
2019-02-12 22:29:26 -0700

    Add stretch hog

--- js/plugins.js
+++ js/plugins.js
@@ -5,5 +5,6 @@ var $plugins =
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
 {"name":"MOG_Weather_EX","status":true,"description":"(v3.1 *) Adiciona novos efeitos de climas.","parameters":{"Battle Weather":"true"}},
-{"name":"MOG_Footsteps","status":true,"description":"(v1.1) Apresenta as pegadas do character em determinadas regiões.","parameters":{"StepFoot Terrain ID":"1","WaterSlash Terrain ID":"2","Duration":"60","X-Axis Offset":"0","Y-Axis Offset":"5","Play WaterSplash SE":"false","SE File Name":"Ice1"}}
+{"name":"MOG_Footsteps","status":true,"description":"(v1.1) Apresenta as pegadas do character em determinadas regiões.","parameters":{"StepFoot Terrain ID":"1","WaterSlash Terrain ID":"2","Duration":"60","X-Axis Offset":"0","Y-Axis Offset":"5","Play WaterSplash SE":"false","SE File Name":"Ice1"}},
+{"name":"tile_changer_(mv)","status":true,"description":"Change tiles on map or copy tiles from another map","parameters":{}}
 ];
