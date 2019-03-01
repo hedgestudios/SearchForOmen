379d901d7841684746ff65fa1da4b1d3859be254
Wok Seng <wok@ualberta.ca>
2019-03-01 12:01:06 -0700

    Fix minor changes

--- js/plugins.js
+++ js/plugins.js
@@ -23,5 +23,6 @@ var $plugins =
 {"name":"-SHATTEREFFECT","status":false,"description":"----------------------------------------------------------------------------------","parameters":{}},
 {"name":"MOG_CharShatterEffect","status":true,"description":"(v1.1) Cria o efeito de shatter nos sprites dos characters.","parameters":{}},
 {"name":"-TIMER","status":false,"description":"----------------------------------------------------------------------------------","parameters":{}},
-{"name":"MOG_VisualTimer","status":true,"description":"(v1.1) Apresenta o contador de tempo através de imagens.","parameters":{"Number Visible":"true","Gauge Visible":"true","X-Axis":"310","Y-Axis":"64","Seconds X-Axis":"132","Seconds Y-Axis":"0","Minutes X-Axis":"82","Minutes Y-Axis":"0","Gauge X-Axis":"11","Gauge Y-Axis":"34","Auto Fade":"true","Fade Max":"120","Gauge Angle":"0","Number Angle":"0"}}
+{"name":"MOG_VisualTimer","status":true,"description":"(v1.1) Apresenta o contador de tempo através de imagens.","parameters":{"Number Visible":"true","Gauge Visible":"true","X-Axis":"310","Y-Axis":"64","Seconds X-Axis":"132","Seconds Y-Axis":"0","Minutes X-Axis":"82","Minutes Y-Axis":"0","Gauge X-Axis":"11","Gauge Y-Axis":"34","Auto Fade":"true","Fade Max":"120","Gauge Angle":"0","Number Angle":"0"}},
+{"name":"MOG_EventIndicators","status":true,"description":"(v1.0) Apresenta indicadores animados acima do evento.","parameters":{"Default Font Size":"16","Default X - Axis":"0","Default Y - Axis":"0"}}
 ];
